6d5a414e1538845d70a56836056665e4cfb2a6c6
Emdadul_Islam <emdadulislam439@gmail.com>
2023-06-04 14:54:39 +0600

    created action

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const initialState = {
   noOfCakes: 10,
 };
 
-// actions
+// actions-> action_name, actions
+const BUY_CAKE = "BUY_CAKE";
+function buyCakes() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action"
+  }
+}
+
 // reducers
 // store
-// 
+//
 
 console.log("From INDEX.JS");
